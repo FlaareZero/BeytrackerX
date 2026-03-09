5205c8695557970353405d4a4c619aef80b43cf8
Mattia Rugna <60710669+FlaareZero@users.noreply.github.com>
2026-03-09 17:19:04 +0100

    Update service worker cache version and clean up
    Updated cache version to 'beytrackerx-v7' and removed unused code.

--- sw.js
+++ sw.js
@@ -1,36 +1,4 @@
-const CACHE = 'beytrackerx-v6';
-const ASSETS = [
-  './',
-  './index.html',
-  './data.js',
-  './icons.js',
-  'https://unpkg.com/react@18/umd/react.production.min.js',
-  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
-  'https://unpkg.com/@babel/standalone/babel.min.js'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request))
-  );
-});
-const CACHE = 'beytrackerx-v5';
+const CACHE = 'beytrackerx-v7';
 const ASSETS = [
   './',
   './index.html',
